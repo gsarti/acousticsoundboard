f3b7bf764e43a24b0a09d865152e0d76de295e57
Tristan <tris790@gmail.com>
2017-11-30 16:43:08 -0500

    Ajout du layout avec des cards + popup pour les cards

--- app/src/main/java/com/cegepsth/asb/acousticsoundboard/SoundboardAdapter.java
+++ app/src/main/java/com/cegepsth/asb/acousticsoundboard/SoundboardAdapter.java
@@ -4,11 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,7 +39,7 @@ public class SoundboardAdapter extends RecyclerView.Adapter<SoundboardAdapter.So
         Sound s = mListeSound.get(position);
         if (s == null){ return;}
         holder.mNameSound.setText(s.getName());
-        holder.mDurationSound.setText(s.getDuration());
+        holder.mDurationSound.setText(s.getDuration() + "");
         holder.itemView.setTag(s.getPath());
     }
 
@@ -49,12 +53,14 @@ public class SoundboardAdapter extends RecyclerView.Adapter<SoundboardAdapter.So
         public TextView mNameSound;
         public TextView mDurationSound;
         public ImageView mImageSound;
+        public ImageButton mImgMore;
 
         public SoundViewHolder(View view){
             super(view);
             mNameSound = itemView.findViewById(R.id.tv_name_sound);
             mDurationSound = itemView.findViewById(R.id.tv_duration_sound);
             mImageSound = itemView.findViewById(R.id.iv_image_sound);
+            mImgMore = itemView.findViewById(R.id.imgMore);
             view.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -63,6 +69,26 @@ public class SoundboardAdapter extends RecyclerView.Adapter<SoundboardAdapter.So
                     intent.putExtra("song", (String)view.getTag());
                     intent.setAction(AudioTask.ACTION_PLAY_SOUND);
                     context.startService(intent);
+                }
+            });
+            mImgMore.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Context ctx = view.getContext();
+                    PopupMenu menu = new PopupMenu(ctx, view);
+                    menu.getMenuInflater()
+                            .inflate(R.menu.soundboard, menu.getMenu());
+
+                    //registering popup with OnMenuItemClickListener
+                    menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        public boolean onMenuItemClick(MenuItem item) {
+                            Context c = mNameSound.getContext();
+                            Toast.makeText(c, "Clicked", Toast.LENGTH_SHORT).show();
+                            return true;
+                        }
+                    });
+
+                    menu.show();
                 }
             });
         }
